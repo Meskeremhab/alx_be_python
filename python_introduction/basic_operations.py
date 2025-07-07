01956e3a87028212b9fdef60aa9c79e238639438
mhabtom <meski123habtom@gmail.com>
2025-07-07 15:46:34 +0200

    basic operations new

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,7 +2,7 @@
 #In this file, you will define two variables: number1 and number2, 
 #with the values 10 and 5, respectively.
 number1 = 10
-number2 =  5
+number2 = 5
 
 #Calculate the sum, difference (by subtracting number2 from number1),
 # and product of number1 and number2.
